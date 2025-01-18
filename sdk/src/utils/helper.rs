f6c1fcda59198a4d5f32e4c0e1f41dd95ac82741
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2025-01-18 04:53:32 +0000

    chore; more of this progress

--- sdk/src/utils/helper.rs
+++
@@ -1,5 +0,0 @@
-use uuid::Uuid;
-
-pub fn sqlite_test_document(id: Uuid) -> String {
-    format!("sqlite://tests/sqlite/tests-{id}.sqlite?mode=rwc")
-}
--- sdk/src/utils/mod.rs
+++ sdk/src/utils/mod.rs
@@ -1,7 +1,4 @@
-// pub mod error;
-// pub mod helper;
-// // pub mod kafka;
-// pub mod redis;
+pub mod kafka;
 pub mod redis;
 pub mod utility;
 pub mod error;
