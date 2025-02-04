13999ed0018f42c9456277ad0ebb0f0c8bac022b
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2025-02-04 22:55:29 +0000

    chore; here

--- account/src/repository/channel_user.rs
+++ account/src/repository/channel_user.rs
@@ -1,21 +1,30 @@
-use chrono::Utc;
-use sea_orm::{ActiveModelTrait, ColumnTrait, Condition, DbErr, EntityTrait, IntoActiveModel};
-use sea_orm::ActiveValue::Set;
-use tracing::{debug, error};
 use {
-    crate::connections::db::DB,
+    crate::{connections::db::DB, repository::channel_user},
     async_trait::async_trait,
+    chrono::Utc,
     sdk::{
         errors::RepoError,
-        models::others::auth::channel::{CreateChannelUser, UpdateChannelUser},
+        models::{
+            db::account::{
+                user,
+                workspace_user::{
+                    ActiveModel, Column, Entity as ChannelUserEntity,
+                    Model as ChannelUser,
+                },
+            },
+            others::{
+                auth::channel::{CreateChannelUser, UpdateChannelUser},
+                Paginated, Pagination,
+            },
+        },
     },
+    sea_orm::{
+        entity::*, query::*, ActiveModelTrait, ActiveValue::Set, ColumnTrait,
+        Condition, DbErr, EntityTrait, IntoActiveModel,
+    },
+    tracing::{debug, error},
     uuid::Uuid,
 };
-use sdk::models::db::account::user;
-use sdk::models::db::account::workspace_user::{Model as ChannelUser, ActiveModel, Entity as ChannelUserEntity, Column};
-use sdk::models::others::{Paginated, Pagination};
-use crate::repository::channel_user;
-use sea_orm::{entity::*, query::*};
 
 #[async_trait]
 pub trait ChannelUserRepository {
@@ -34,7 +43,12 @@ pub trait ChannelUserRepository {
     async fn delete(&self, id: Uuid, request_id: Uuid)
         -> Result<(), RepoError>;
 
-    async fn get(&self, id: Uuid, pagination: Pagination, request_id: Uuid) -> Result<Paginated<Vec<ChannelUser>>, RepoError>;
+    async fn get(
+        &self,
+        id: Uuid,
+        pagination: Pagination,
+        request_id: Uuid,
+    ) -> Result<Paginated<Vec<ChannelUser>>, RepoError>;
 
     async fn get_by_id(
         &self,
@@ -48,13 +62,24 @@ pub struct ChannelUserRepo(DB);
 #[async_trait]
 impl ChannelUserRepository for ChannelUserRepo {
     #[tracing::instrument(name = "ChannelUserRepo::create", skip(self))]
-    async fn create(&self, payload: CreateChannelUser, request_id: Uuid) -> Result<ChannelUser, RepoError> {
+    async fn create(
+        &self,
+        payload: CreateChannelUser,
+        request_id: Uuid,
+    ) -> Result<ChannelUser, RepoError> {
         todo!()
     }
 
     #[tracing::instrument(name = "ChannelUserRepo::update", skip(self))]
-    async fn update(&self, payload: UpdateChannelUser, request_id: Uuid) -> Result<ChannelUser, RepoError> {
-        debug!("Updating channel_user by id: {:?}, request_id {}", payload, request_id);
+    async fn update(
+        &self,
+        payload: UpdateChannelUser,
+        request_id: Uuid,
+    ) -> Result<ChannelUser, RepoError> {
+        debug!(
+            "Updating channel_user by id: {:?}, request_id {}",
+            payload, request_id
+        );
 
         let model: ActiveModel = payload.into();
 
@@ -69,8 +94,15 @@ impl ChannelUserRepository for ChannelUserRepo {
     }
 
     #[tracing::instrument(name = "ChannelUserRepo::delete", skip(self))]
-    async fn delete(&self, id: Uuid, request_id: Uuid) -> Result<(), RepoError> {
-        debug!("Deleting channel_user by id: {}, request_id {}", id, request_id);
+    async fn delete(
+        &self,
+        id: Uuid,
+        request_id: Uuid,
+    ) -> Result<(), RepoError> {
+        debug!(
+            "Deleting channel_user by id: {}, request_id {}",
+            id, request_id
+        );
 
         let mut channel_user =
             self.get_by_id(request_id, id).await?.into_active_model();
@@ -80,7 +112,10 @@ impl ChannelUserRepository for ChannelUserRepo {
         let res = channel_user.update(&self.0.connection).await;
 
         if let Err(DbErr::Exec(err)) = &res {
-            error!(error = &err.to_string(), "Failed to find channel_user by mail");
+            error!(
+                error = &err.to_string(),
+                "Failed to find channel_user by mail"
+            );
             return Err(RepoError::SomethingWentWrong);
         }
 
@@ -88,8 +123,16 @@ impl ChannelUserRepository for ChannelUserRepo {
     }
 
     #[tracing::instrument(name = "ChannelUserRepo::get", skip(self))]
-    async fn get(&self, id: Uuid, pagination: Pagination, request_id: Uuid) -> Result<Paginated<Vec<ChannelUser>>, RepoError> {
-        debug!("getting channel users by id: {}, request_id {}", id, request_id);
+    async fn get(
+        &self,
+        id: Uuid,
+        pagination: Pagination,
+        request_id: Uuid,
+    ) -> Result<Paginated<Vec<ChannelUser>>, RepoError> {
+        debug!(
+            "getting channel users by id: {}, request_id {}",
+            id, request_id
+        );
 
         let result = ChannelUserEntity::find()
             .limit(Some(pagination.page_size)) // Set limit
@@ -98,22 +141,29 @@ impl ChannelUserRepository for ChannelUserRepo {
             .await;
 
         if let Err(DbErr::Query(err)) = &result {
-            error!(error = &err.to_string(), "Failed to find channel_user by id");
+            error!(
+                error = &err.to_string(),
+                "Failed to find channel_user by id"
+            );
             return Err(RepoError::SomethingWentWrong);
         }
 
         let count = ChannelUserEntity::find().count(&self.0.connection).await;
 
         if let Err(DbErr::Query(err)) = &count {
-            error!(error = &err.to_string(), "Failed to find channel_user by id");
+            error!(
+                error = &err.to_string(),
+                "Failed to find channel_user by id"
+            );
             return Err(RepoError::SomethingWentWrong);
         }
 
         let count = count.unwrap();
 
-        let total_pages = (count + pagination.page_size - 1) / pagination.page_size;
+        let total_pages =
+            (count + pagination.page_size - 1) / pagination.page_size;
 
-        let paginated = Paginated{
+        let paginated = Paginated {
             result: result.unwrap(),
             total_pages,
             current_page: 0,
@@ -125,13 +175,25 @@ impl ChannelUserRepository for ChannelUserRepo {
     }
 
     #[tracing::instrument(name = "ChannelUserRepo::get_by_id", skip(self))]
-    async fn get_by_id(&self, id: Uuid, request_id: Uuid) -> Result<ChannelUser, RepoError> {
-        debug!("Getting channel_user by id: {}, with request id: {}", id, request_id);
+    async fn get_by_id(
+        &self,
+        id: Uuid,
+        request_id: Uuid,
+    ) -> Result<ChannelUser, RepoError> {
+        debug!(
+            "Getting channel_user by id: {}, with request id: {}",
+            id, request_id
+        );
 
-        let result = ChannelUserEntity::find_by_id(id).one(&self.0.connection).await;
+        let result = ChannelUserEntity::find_by_id(id)
+            .one(&self.0.connection)
+            .await;
 
         if let Err(DbErr::Exec(err)) = &result {
-            error!(error = &err.to_string(), "Failed to find channel_user by id");
+            error!(
+                error = &err.to_string(),
+                "Failed to find channel_user by id"
+            );
             return Err(RepoError::SomethingWentWrong);
         }
 
