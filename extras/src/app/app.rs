c48bb41cebbd61bf9442a9acb627135a4bad7da4
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2025-02-17 19:04:34 +0000

    chore; clear

--- extras/src/app/app.rs
+++ extras/src/app/app.rs
@@ -9,7 +9,6 @@ use {
             profile_media::{ProfileMediaRepo, ProfileMediaRepository},
             short_url::{ShortUrlRepo, ShortUrlRepository},
             short_url_track::{ShortUrlTrackRepo, ShortUrlTrackRepository},
-            user::{UserRepo, UserRepository},
         },
     },
     uuid::Uuid,
@@ -21,7 +20,6 @@ pub struct App {
     pub downstream: Box<dyn Downstream + Sync + Send>,
     pub profile_media_repo: Box<dyn ProfileMediaRepository + Sync + Send>,
     pub chat_media_repo: Box<dyn ChatMediaRepository + Sync + Send>,
-    pub user_repo: Box<dyn UserRepository + Sync + Send>,
     pub short_url_repo: Box<dyn ShortUrlRepository + Sync + Send>,
     pub short_url_track_repo: Box<dyn ShortUrlTrackRepository + Send + Sync>,
 }
@@ -30,7 +28,6 @@ impl App {
     pub async fn new(c: &Config) -> Self {
         let db = DB::new(&c).await.unwrap();
 
-        let user = UserRepo::new(db.clone());
         let short_repo = ShortUrlRepo::new(db.clone());
         let short_track = ShortUrlTrackRepo::new(db.clone());
         let profile_media_repo = ProfileMediaRepo::new(db.clone());
@@ -38,7 +35,6 @@ impl App {
 
         Self {
             db,
-            user_repo: Box::new(user),
             config: Config::new(),
             downstream: Box::new(DownstreamImpl::new()),
             short_url_repo: Box::new(short_repo),
